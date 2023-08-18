0a21a1d4d643b83b042d54548d9809e966cfda3c
zhoulei-Ckjs <56968749@qq.com>
2023-08-18 11:43:46 +0800

    [内核]-GDT::段描述符

--- include/linux/head.h
+++ include/linux/head.h
@@ -12,4 +12,23 @@ typedef struct global_descriptor_table_pointer
 }gdt_ptr_t;
 #pragma pack()
 
+/**
+ * @brief 段描述符
+ */
+typedef struct gdt_item
+{
+    unsigned short limit_low;           ///< 段界限 0 ~ 15 位
+    unsigned int base_low : 24;         ///< 基地址 0 ~ 23 位 16M
+    unsigned char type : 4;             ///< 段类型
+    unsigned char segment : 1;          ///< 1 表示代码段或数据段，0 表示系统段
+    unsigned char DPL : 2;              ///< Descriptor Privilege Level 描述符特权等级 0 ~ 3
+    unsigned char present : 1;          ///< 存在位，1 在内存中，0 在磁盘上
+    unsigned char limit_high : 4;       ///< 段界限 16 ~ 19
+    unsigned char available : 1;        ///< 该位是用户位，可以被用户自由使用。
+    unsigned char long_mode : 1;        ///< 64 位扩展标志
+    unsigned char big : 1;              ///< 该位为 0 表示这是一个 16 位的段，1 表示这是一个 32 位段。
+    unsigned char granularity : 1;      ///< 粒度 4KB 或 1B，0 表示以字节为单位，20根地址总线就是 2 ^ 20 = 1M；1表示以4K为单位，20跟地址总线就是1M * 4K = 4G。
+    unsigned char base_high;            ///< 基地址 24 ~ 31 位
+} __attribute__((packed)) gdt_item_t;   ///< 按紧凑方式对结构体进行内存对齐，取消默认的对齐填充
+
 #endif //OS_HEAD_H
--- kernel/gdt.c
+++ kernel/gdt.c
@@ -5,9 +5,9 @@
 gdt_ptr_t gdt_ptr;              ///< 存储 gdt 信息的全局结构体
 
 /**
- * @brief 用于存储 gdt 表中每一个段描述符，每个描述符占 8 字符。
+ * @brief 全局描述符表。用于存储段描述符，每个描述符占 8 字符。
  */
-u64 gdt[GDT_SIZE];
+gdt_item_t gdt[GDT_SIZE];
 
 void gdt_init()
 {
@@ -15,6 +15,12 @@ void gdt_init()
     /// 加载 gdt 表信息，保存到 gdt_ptr 中
     __asm__ volatile ("sgdt gdt_ptr;");
     memcpy(gdt, gdt_ptr.base, gdt_ptr.limit + 1);   ///< 将 gdt 表的数据拷贝出来
-    printk("%X\n", (gdt[1]) & 0xffff);          /// 0xffff
-    printk("%X\n", (gdt[1] >> 40) & 0xff);
+
+    printk("gdt_item_t size = %d\n", sizeof(gdt_item_t));
+    printk("%x\n", gdt[1].limit_low);               ///< 应该是 0xffff
+    printk("%x\n", gdt[1].base_low);                ///< 应该是 0x0000
+    printk("%x\n", gdt[1].base_low);                ///< 应该是 0x0000
+    printk("%x\n", gdt[1].segment);                 ///< 应该是 0x1
+    printk("%x\n", gdt[1].DPL);                     ///< 应该是 0x0
+    printk("%x\n", gdt[1].present);                 ///< 应该是 0x1
 }
