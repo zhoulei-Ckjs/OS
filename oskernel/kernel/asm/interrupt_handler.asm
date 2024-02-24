d2a4139191d337c74034ccde133dcaea8297d746
zhoulei <56968749@qq.com>
2024-02-24 08:01:25 +0800

    [内核]-IDT::定义中断处理宏

--- oskernel/kernel/asm/interrupt_handler.asm
+++ oskernel/kernel/asm/interrupt_handler.asm
@@ -41,5 +41,27 @@ keymap_handler_entry:
 
     iret            ; 拿出 CPU 自动压入的寄存器的值，返回进入中断前的程序
 
+; 定义 INTERRUPT_HANDLER 宏，具备 1 个参数。
+%macro INTERRUPT_HANDLER 1
+global interrupt_handler_%1
+interrupt_handler_%1:
+    pushad          ; pushad 是一个 x86 架构汇编指令，用于将所有通用寄存器中的值按照固定顺序依次压入堆栈（栈）中
+                    ; EAX
+                    ; ECX
+                    ; EDX
+                    ; EBX
+                    ; ESP（当前栈指针）
+                    ; EBP（栈基指针）
+                    ; ESI
+                    ; EDI
+    push %1
+    call exception_handler
+    add esp, 4      ; 平栈，因为 push %1 压入了 1 个参数
+
+    popad           ; 推出所有压入寄存器的值
+
+    iret            ; 中断返回
+%endmacro
+
 msg:
     db "interrupt_handler", 10, 0
