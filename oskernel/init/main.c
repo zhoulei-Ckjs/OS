6e6c44c880bf929225a7462c881af856febf071b
zhoulei <56968749@qq.com>
2024-03-04 06:50:38 +0800

    [内核]-IDT::时钟中断::初始化

--- oskernel/init/main.c
+++ oskernel/init/main.c
@@ -3,11 +3,14 @@
 #include "../include/string.h"
 #include "../include/linux/traps.h"
 
+extern void clock_init();
+
 void kernel_main(void)
 {
     console_init();
     gdt_init();
     idt_init();
+    clock_init();
 
     __asm__("sti;");        ///< 开中断
 
--- oskernel/kernel/chr_drv/clock.c
+++ oskernel/kernel/chr_drv/clock.c
@@ -1,6 +1,14 @@
 #include "../../include/linux/kernel.h"
 #include "../../include/linux/traps.h"
 
+/**
+ * @brief 时钟中断初始化，控制时钟中断频率
+ */
+void clock_init()
+{
+
+}
+
 void clock_handler(int idt_index)
 {
     send_eoi(idt_index);
