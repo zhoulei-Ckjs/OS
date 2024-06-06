4773791ba19b51e0507b70fee486fa4c49c40ba4
zhoulei <56968749@qq.com>
2024-06-07 07:08:20 +0800

    [内核]-进程::定义进程结构

--- oskernel/include/linux/task.h
+++ oskernel/include/linux/task.h
@@ -1,6 +1,26 @@
 #ifndef OS_TASK_H
 #define OS_TASK_H
 
+#include "mm.h"
+
+/**
+ * @brief 进程描述信息
+ */
+typedef struct task_t
+{
+    int pid;        ///< 进程 id
+    int cr3;        ///< 存储页表起始地址
+}task_t;
+
+/**
+ * @brief 进程
+ */
+typedef union task_union_t
+{
+    task_t task;                ///< 进程描述信息
+    char stack[PAGE_SIZE];      ///< 进程栈空间
+}task_union_t;
+
 /**
  * @brief 进程初始化
  */
--- oskernel/kernel/task.c
+++ oskernel/kernel/task.c
@@ -1,11 +1,15 @@
 #include "../include/linux/task.h"
+#include "../include/string.h"
 
 void create_task()
 {
+    task_union_t* task = (task_union_t*)get_free_page();
+    memset(task, 0, PAGE_SIZE);
+
     /**
      * 每个进程都要有自己的虚拟页表
      */
-    virtual_memory_init();
+    task->task.cr3 = virtual_memory_init();
 }
 
 void task_init()
